fa43d3f746b473c3cbd3b8a047d54c8ccec339dd
Ted Goddard <ted.goddard@robotsandpencils.com>
2018-06-06 15:35:25 -0600

    use ELMo dimension

--- allennlp/service/expert_extension.py
+++ allennlp/service/expert_extension.py
@@ -15,13 +15,16 @@ elmoEmbedder = ElmoEmbedder()
 def fetch_elmo() -> Response:  # pylint: disable=unused-variable
     req = request.get_json(silent=True, force=True)
     sentences = req.get('sentences')
+    dimension = req.get('dimension', 1024)
     result = []
     try:
-        logger.info("Determining ELMo vectors for %s" % str(sentences)[:1000])
+        logger.info("Determining ELMo vectors for %s sentences %s" %
+                    (len(sentences), str(sentences)[:1000]))
         tokenized = [tokenize(sentence) for sentence in sentences]
         embedded = elmoEmbedder.embed_batch(tokenized)
         for (sentence, tokens, embedding) in zip(sentences, tokenized,
                                                  embedded):
+            vectors = project(dimension, embedding)
             result += [{
                 "sentence": sentence,
                 "tokens": tokens,
@@ -34,3 +37,7 @@ def fetch_elmo() -> Response:  # pylint: disable=unused-variable
 
 def tokenize(sentence):
     return [t.text for t in tokenizer.tokenize(sentence)]
+
+
+def project(dimension, embedding):
+    return embedding[:, :, :dimension]
